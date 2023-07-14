69fee509d6e36e669088d5b217ee6ccc01bfb415
jonataserpa <jonataserpa@gmail.com>
2023-07-14 10:10:14 -0300

    fix: port deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { app } = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://154.56.41.149/:${port}`);
 });
